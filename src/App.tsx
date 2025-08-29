61e766b02ee0be8cae5d1584beb9a71d1dc1ffd4
Advansed <atlasov.n.r@gmail.com>
2025-08-29 16:12:18 +0900

    11

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Page from './pages/Page';
 import { ServerConnectionGuard } from './components/ServerConnectionGuard/ServerConnectionGuard';
 import { ToastProvider } from './components/Toast';
 import ConnectionStatus from './components/ConnectionStatus/ConnectionStatus';
+import Login from './components/Login/Login';
+import { useLogin } from './components/Store/useLogin';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,28 +34,38 @@ import './theme/variables.css';
 
 setupIonicReact();
 
+const AppContent: React.FC = () => {
+  const { auth } = useLogin();
+
+  return (
+    <ServerConnectionGuard>
+      {auth ? (
+        <IonReactRouter>
+          <IonSplitPane contentId="main">
+            <Menu />
+            <IonRouterOutlet id="main">
+              <Route path="/" exact={true}>
+                <Redirect to="/folder/Inbox" />
+              </Route>
+              <Route path="/folder/:name" exact={true}>
+                <Page />
+              </Route>
+            </IonRouterOutlet>
+          </IonSplitPane>
+        </IonReactRouter>
+      ) : (
+        <Login />
+      )}
+      <ConnectionStatus />
+    </ServerConnectionGuard>
+  );
+};
+
 const App: React.FC = () => {
   return (
     <IonApp>
       <ToastProvider>
-        <ServerConnectionGuard>
-
-          <IonReactRouter>
-            <IonSplitPane contentId="main">
-              <Menu />
-              <IonRouterOutlet id="main">
-                <Route path="/" exact={true}>
-                  <Redirect to="/folder/Inbox" />
-                </Route>
-                <Route path="/folder/:name" exact={true}>
-                  <Page />
-                </Route>
-              </IonRouterOutlet>
-            </IonSplitPane>
-          </IonReactRouter>
-          <ConnectionStatus />
-          
-        </ServerConnectionGuard>
+        <AppContent />
       </ToastProvider>
     </IonApp>
   );
